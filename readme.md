67b6d09b69b65e9610f5c4914b4be611aaacc716
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2025-05-05 00:29:01 +0200

    doc comments

--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ is the official spelling, not Nunchu_c_k.
 ## Introduction
 
 I found out a Nunchuk is actually an I2C device.
-It has I2C address 0x52 and accepts the fast protocol (400kHz).
+It has I2C address 0x52 and accepts the fast protocol (100kHz).
 It runs on 3.3V - although rumors has it that it works on 5V too.
 
 The device needs hardly any writing, mostly just reading the sensors.
@@ -26,9 +26,20 @@ Information from
  - [WiiChuck](https://github.com/madhephaestus/WiiChuck)
 
 
+## Clones
+
+To my surprise clones are better in all (?) I2C aspects.
+
+- My clone accepts 400kHz I2C, the real Nunchuk needs 100kHz.
+- The joystick values of the clone use the full -128 to +127 range (not -104 to +86 of the real one).
+- Also accelerometer meter range is reduced -192 to 197 for the real one (-512 to +511).
+- An I2C read transaction needs a stop/start, clones accept the normal repeated-start.
+- The real Nunchuk needs some delays between i2C transactions, my clone not.
+
+
 ## I2C device
 
-The pinout of the Nunchuk is not hard, but most drawings you find on the internet
+The pin-out of the Nunchuk is not hard, but most drawings you find on the internet
 don't tell whether you are looking at the plug of the Nunchuk or the outlet of the Wii controller.
 To avoid all confusion, here a photo of the Nunchuk plug, with the meaning of the pins.
 Also note the notch on the bottom side, ensuring the plug goes in one-way only.
--- src/nunchuk.h
+++ src/nunchuk.h
@@ -7,30 +7,45 @@
 #include <Wire.h>    // TwoWire
 
 
-// The Nunchuk ID - hopefully returned by nunchuk_id()
+// The Nunchuk ID - hopefully returned by `nunchuk_id()`.
 #define NUNCHUK_ID              0xA4200000
 
 
+// Initializes the Nunchuk (and I2C if `wire==0`) - returns `true` if Nunchuk found.
+int  nunchuk_begin(TwoWire *wire=0);
+// Returns the Nunchuk id.
+uint32_t nunchuk_id();              
 
-
-int  nunchuk_begin(TwoWire *wire=0);// Initializes the Nunchuk (and wire if 0 passed) - returns true if Nunchuk found
-uint32_t nunchuk_id();              // Returns the Nunchuk id
-
-int  nunchuk_scan();                 // Read the raw sensor values from the Nunchuk into the Arduino memory. Returns true if successful.
+// Read the raw sensor values from the Nunchuk into the Arduino memory. Returns `true` if successful.
+int  nunchuk_scan();                 
      
-int  nunchuk_buttonZ();              // Returns the state of Z button - from latest nunchuk_scan().
-int  nunchuk_buttonC();              // Returns the state of C button - from latest nunchuk_scan().
-int  nunchuk_joystickX();            // Returns the X-value of the joystick (-128..0..+127) - from latest nunchuk_scan().
-int  nunchuk_joystickY();            // Returns the Y-value of the joystick (-128..0..+127)  - from latest nunchuk_scan().
-int  nunchuk_accelX();               // Returns the X-value of the accelerometer (-512..0..+511) - from latest nunchuk_scan().
-int  nunchuk_accelY();               // Returns the Y-value of the accelerometer (-512..0..+511) - from latest nunchuk_scan().
-int  nunchuk_accelZ();               // Returns the Z-value of the accelerometer (-512..0..+511) - from latest nunchuk_scan().
+// Returns the state of Z button - from latest `nunchuk_scan()`.
+int  nunchuk_buttonZ();              
+// Returns the state of C button - from latest `nunchuk_scan()`.
+int  nunchuk_buttonC();              
+// Returns the X-value of the joystick (-128..0..+127) - from latest `nunchuk_scan()`.
+int  nunchuk_joystickX();            
+// Returns the Y-value of the joystick (-128..0..+127)  - from latest `nunchuk_scan()`.
+int  nunchuk_joystickY();            
+// Returns the X-value of the accelerometer (-512..0..+511) - from latest `nunchuk_scan()`.
+int  nunchuk_accelX();               
+// Returns the Y-value of the accelerometer (-512..0..+511) - from latest `nunchuk_scan()`.
+int  nunchuk_accelY();               
+// Returns the Z-value of the accelerometer (-512..0..+511) - from latest `nunchuk_scan()`.
+int  nunchuk_accelZ();               
 
 
-void nunchuk_lpf(int factor=750);    // Call once after each nunchuk_scan() to update LPF accumulators. factor= 0..1024; 0 is no filtering (always raw, fast but jumpy)
-int  nunchuk_accelXlpf();            // Returns the low-pass-filtered X-value of the accelerometer - from latest nunchuk_scan()/nunchuk_lpf() pair.
-int  nunchuk_accelYlpf();            // Returns the low-pass-filtered Y-value of the accelerometer - from latest nunchuk_scan()/nunchuk_lpf() pair.
-int  nunchuk_accelZlpf();            // Returns the low-pass-filtered Z-value of the accelerometer - from latest nunchuk_scan()/nunchuk_lpf() pair.
+// This driver includes a low-pass-filter (LPF) for the accelerometers.
+// Call this function once, after each `nunchuk_scan()` call, to update LPF accumulators. 
+// Then use `nunchuk_accel[XYZ]lpf` instead of `nunchuk_accel[XYZ]`.
+// Parameter `factor`= 0..1024; 0 is no filtering (always raw, fast but jumpy).
+void nunchuk_lpf(int factor=750);
+// Returns the low-pass-filtered X-value of the accelerometer (-512..0..+511) - from latest nunchuk_scan()/nunchuk_lpf() pair.
+int  nunchuk_accelXlpf();
+// Returns the low-pass-filtered Y-value of the accelerometer (-512..0..+511) - from latest nunchuk_scan()/nunchuk_lpf() pair.
+int  nunchuk_accelYlpf();
+// Returns the low-pass-filtered Z-value of the accelerometer (-512..0..+511) - from latest nunchuk_scan()/nunchuk_lpf() pair.
+int  nunchuk_accelZlpf();
 
 
 #endif
